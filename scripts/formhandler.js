d1a7672752ebe601a3be414e8e892d361f13c0bc
LUAN\luankhong <lkdrumandbass@csu.fullerton.edu>
2017-04-02 18:22:30 -0700

    New  Upload

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -1,6 +1,40 @@
-// write code for email validation and form handling stuff
-(function (window) {
-  'use strict';
-  var App = window.App;
+(function(window) {
+    'use strict';
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    function FormHandler(selector) {
+        if (!selector) {
+            throw new Error('No selector provided');
+        }
+
+        this.$formElement = $(selector);
+
+        if (this.$formElement.length === 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    FormHandler.prototype.addSubmitHandler = function(fn) {
+        console.log('Setting submit handler for form');
+        this.$formElement.on('submit', function(event) {
+            event.preventDefault();
+
+            var data = {};
+
+            $(this).serializeArray().forEach(function(item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+            });
+            fn(data)
+            .then(function() {
+                this.reset();
+                this.elements[0].focus();
+            }.bind(this));
+        });
+    };
+
+    App.FormHandler = FormHandler;
+    window.App = App;
 
 })(window);
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,33 @@
-(function (window) {
-  'use strict';
-  var App = window.App;
-  var Course = App.Course;
-  var RemoteDB = App.RemoteDB;
-  var Course1 = new Course('CPSC473', new RemoteDB());
-  var Course2 = new Course('CPSC466', new RemoteDB());
+(function(window) {
+    'use strict';
+    var FORM_SELECTOR = '[post-topic="form"]';
+    var CHECKLIST_SELECTOR = '[post-topic="topiclist"]';
+    var SERVER_URL = 'http://localhost:3002/TopicSubmissions';
+    var App = window.App;
+    var RemoteDataStore = App.RemoteDataStore;
+    var FormHandler = App.FormHandler;
+    var CheckList = App.CheckList;
+    var remoteDS = new RemoteDataStore(SERVER_URL);
+    var checkList = new CheckList(CHECKLIST_SELECTOR, remoteDS);
+    var formHandler = new FormHandler(FORM_SELECTOR);
 
-  window.Course1 = Course1;
-  window.Course2 = Course2;
+    //Project 1 -LOAD UP THE PAGE WITH TOPICS
+    var $ = window.jQuery;
+    $.getJSON(SERVER_URL, function(data) {
+        $(data).each(function(key, value) {
+            checkList.addRow(value);
+        });
+    });
+
+    //Store submission into database upon "Submit" clicked
+    formHandler.addSubmitHandler(function(data) {
+        data.id = data.topic;
+        data.likes = 0;
+        data.dislikes = 0;
+        return remoteDS.add.call(remoteDS, data)
+            .then(function() {
+                checkList.addRow.call(checkList, data);
+            });
+    });
+
 })(window);
